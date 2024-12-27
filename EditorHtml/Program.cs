5aab1ea7cf28dca207391f2ae810fc57a980a71c
Arruda98 <jeffersonarruda98@gmail.com>
2024-12-27 13:11:08 -0300

    Add files via upload

---
+++ EditorHtml/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EditorHtml
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.Show();
+            
+        }
+    }
+}
+
+
